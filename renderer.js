8ab25a72d35a21f657a97b6fa546946dc5b22b8f
HanhMChau <hanhmchau@outlook.com>
2018-04-28 16:11:26 +0700

    Add default color when user has no color role

--- renderer.js
+++ renderer.js
@@ -4,13 +4,18 @@ const Discord = require("discord.js");
 
 let Renderer = function() {
 
+    let DEFAULT_COLOR = 12961221;
+
     let renderStats = (stats, message) => {
         let statString = '`' + stats.join('  ') + '`';
         let statSum = stats.reduce((a, b) => a + b);
 
+        let colorRole = message.member.colorRole;
+        let color = colorRole ? colorRole.color : DEFAULT_COLOR;
+
         const embed = new Discord.RichEmbed()
             .setAuthor('Dice of Bug', message.author.displayAvatarURL)
-            .setColor(message.member.colorRole.color)
+            .setColor(color)
             .setFooter(`Total: ${statSum}`)
             .setDescription(statString);
         message.channel.send({embed});
